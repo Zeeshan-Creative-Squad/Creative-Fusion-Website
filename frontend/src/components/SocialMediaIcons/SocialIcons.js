b535c0cd0ee9242e12e1e4aa5a17890de37bf06d
Muhammad Zeeshan <zeeshan.creativesquad@gmail.com>
2024-05-31 16:35:41 -0400

    dp

--- frontend/src/components/SocialMediaIcons/SocialIcons.js
+++ frontend/src/components/SocialMediaIcons/SocialIcons.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { ReactComponent as FacebookIcon } from 'file:///C:/Users/Hunain/Documents/CreativeFusion-master/frontend/public/images/icons/rc-fb-icon.svg';
-import { ReactComponent as InstagramIcon } from 'file:///C:/Users/Hunain/Documents/CreativeFusion-master/frontend/public/images/icons/rc-insta-icon.svg';
-import { ReactComponent as TwitterIcon } from 'file:///C:/Users/Hunain/Documents/CreativeFusion-master/frontend/public/images/icons/rc-x-icon.svg';
+import { ReactComponent as FacebookIcon } from '../../../public/images/icons/rc-fb-icon.svg';
+import { ReactComponent as InstagramIcon } from '../../../public/images/icons/rc-insta-icon.svg';
+import { ReactComponent as TwitterIcon } from '../../../public/images/icons/rc-x-icon.svg';
 
 
 
