8f7ecb5d2fda62e257e74b5567e3dfd580fd52a9
Muhammad Zeeshan <zeeshan.creativesquad@gmail.com>
2024-08-20 14:57:41 -0400

    Slightly Changes

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -20,7 +20,7 @@ function About() {
                 subHeading=""
                 heading="Meet Taha Sheikh, "
                 headingAfterBr="  CEO of Creative Fusion (Pakistan & MENA Region) "
-                paragraph="As the founder of Creative Fusion, I am passionate about helping businesses thrive in the digital world. With a background in technology and a keen eye for creativity, I established Creative Fusion to provide innovative digital solutions customized to our clients' needs. <br><br> At Creative Fusion, we believe in the power of collaboration and excellence. Our dedicated team works tirelessly to bring our clients' visions to life, ensuring every project is a success story. <br><br> With years of experience and a commitment to delivering exceptional results, Creative Fusion is your trusted partner for all your digital needs. "
+                paragraph="As the founder of Creative Fusion, I am passionate about helping businesses thrive in the digital world. With a background in technology and a keen eye for creativity, I established Creative Fusion to provide innovative digital solutions customized to our clients' needs. At Creative Fusion, we believe in the power of collaboration and excellence. Our dedicated team works tirelessly to bring our clients' visions to life, ensuring every project is a success story. With years of experience and a commitment to delivering exceptional results, Creative Fusion is your trusted partner for all your digital needs. "
                 title="Taha Sheikh"
                 designation="CEO of Creative Fusion."
                 btn={false}
@@ -30,11 +30,11 @@ function About() {
 
             <TextContent
                 img="/images/creatives/about-text-1.png"
-                imgg='/images/creatives/about-text-content-1.png'
+                imgg='/images/creatives/Salman-baig.jfif'
                 subHeading=""
                 heading="Meet Salman Baig, "
                 headingAfterBr=" Founder of Creative Squad Inc"
-                paragraph="As the founder of Creative Squad, I am passionate about helping businesses thrive in the digital world. With a background in technology and a keen eye for creativity, I established Creative Squad to provide innovative digital solutions customized to our clients' needs. <br><br> At Creative Squad, we believe in the power of collaboration and excellence. Our dedicated team works tirelessly to bring our clients' visions to life, ensuring every project is a success story. <br><br> With years of experience and a commitment to delivering exceptional results, Creative Squad is your trusted partner for all your digital needs. "
+                paragraph="As the founder of Creative Squad, I am passionate about helping businesses thrive in the digital world. With a background in technology and a keen eye for creativity, I established Creative Squad to provide innovative digital solutions customized to our clients' needs. At Creative Squad, we believe in the power of collaboration and excellence. Our dedicated team works tirelessly to bring our clients' visions to life, ensuring every project is a success story. With years of experience and a commitment to delivering exceptional results, Creative Squad is your trusted partner for all your digital needs. "
                 btn={false}
                 title='Salman Baig'
                 designation="CEO of Creative Squad Inc."
@@ -42,8 +42,6 @@ function About() {
                 shiftRow={true}
 
             />
-
-            
 
             <div className='box multi-gradient '>
                 <div className='glass-box'>
